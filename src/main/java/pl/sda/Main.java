211b42ff7814de3f695b9a2c773691ff70e1409f
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-07 11:37:39 +0100

    prototype singleton

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -12,5 +12,16 @@ public class Main {
         Message message1 = context.getBean("message1", Message.class);
         System.out.println(message1.getText());
 
+        Message message2 = context.getBean("message1", Message.class);
+        System.out.println(message2.getText());
+        System.out.println(message1 == message2);
+
+
+        ExtraMessage extraMessage1 = context.getBean("message2", ExtraMessage.class);
+        System.out.println(extraMessage1.getText());
+
+        ExtraMessage extraMessage2 = context.getBean("message2", ExtraMessage.class);
+        System.out.println(extraMessage2.getText());
+        System.out.println(extraMessage1 == extraMessage2);
     }
 }
